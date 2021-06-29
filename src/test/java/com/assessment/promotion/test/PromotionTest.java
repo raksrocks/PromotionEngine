84dba652cf71deb26f60416d987bd555f45a1957
raksrocks <rakeshsingarapu@gmail.com>
2021-06-29 15:09:23 +0100

    Added tests for Promotion engine

--- src/test/java/com/assessment/promotion/test/PromotionTest.java
+++ src/test/java/com/assessment/promotion/test/PromotionTest.java
@@ -1,20 +1,44 @@
 package com.assessment.promotion.test;
 
+import com.assessment.promotion.engine.IPromotionEngine;
+import com.assessment.promotion.engine.PromotionEngine;
+import com.assessment.promotion.exception.InvalidProductException;
+import com.assessment.promotion.exception.InvalidPromotionCodeException;
+import com.assessment.promotion.exception.InvalidShoppingCartException;
+import com.assessment.promotion.model.Product;
+import com.assessment.promotion.model.ShoppingCart;
+import com.assessment.promotion.service.PromotionType;
+import com.assessment.promotion.utils.PromotionUtil;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Map;
+
 public class PromotionTest {
+
+    private static IPromotionEngine promoEngine;
+    private static List<PromotionType> promotionsList;
+    private static ShoppingCart cart;
 
     @BeforeAll
     public static void addPromotions(){
+        promoEngine = new PromotionEngine();
+        promotionsList = PromotionUtil.createPromotions();
+        cart = new ShoppingCart();
 
     }
 
     @Test
     @DisplayName("when SinglePromotionType Applied then Validate Cart Value")
-    public void whenSinglePromotionTypeApplied_thenValidateCartValue(){
-
+    public void whenSinglePromotionTypeApplied_thenValidateCartValue() throws InvalidProductException, InvalidShoppingCartException, InvalidPromotionCodeException {
+        Double  expectedPrice = 100.0;
+        cart.clear();
+        cart.setCartContents(Map.of(new Product("A") ,3));
+        Double actualPrice = promoEngine.getDiscountedPrice(cart, promotionsList);
+        Assertions.assertEquals(expectedPrice,actualPrice);
     }
 
     @Test
